4c936213c79148e9050459881c713fcf8f25c340
Luis Penagos <luis@penagos.co>
2022-02-07 18:51:59 -0500

    Add missing link to user profile

--- resources/views/components/post-author.blade.php
+++ resources/views/components/post-author.blade.php
@@ -1,7 +1,7 @@
 <div class="float-end text-muted fs-6 lh-sm bg-light rounded p-2">
     <div class="d-flex">
     <div>
-        <a href="#"><img src="{{ asset('img/avatar-placeholder.svg') }}" class="m-1" width="32" height="32" alt="{{ $post->user->username }}'s profile picture"></a>
+        <a href="{{ route('users.view', $post->user->id) }}"><img src="{{ asset('img/avatar-placeholder.svg') }}" class="m-1" width="32" height="32" alt="{{ $post->user->username }}'s profile picture"></a>
     </div>
     <div class="ml-2">
         <small>Posted @datetime($post->created_at) @edited($post->isEdited())</small><br>
