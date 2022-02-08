0ad40cabbd1e0e5d54cd892dc0902395b94d6e52
Luis Penagos <luis@penagos.co>
2022-02-08 17:59:39 -0500

    URL update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-white">
       <div class="container">
-        <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
+        <a class="navbar-brand" href="{{ URL::to('/') }}">{{ config('app.name') }}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
