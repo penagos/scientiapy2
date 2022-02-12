6d122fec86c46b78948a0e47688436dacb5fa5c8
Luis Penagos <luis@penagos.co>
2022-02-11 21:03:57 -0500

    Add random answer selection to question seeder

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -17,11 +17,18 @@ class QuestionSeeder extends Seeder
     public function run()
     {
         $m = Question::factory(20)->create()->each(function ($question) {
-            Post::factory(random_int(0, 5))->create(['question_id' => $question->id])->each(function ($answer) {
-                Comment::factory(random_int(0, 4))->create(['post_id' => $answer->id]);
-            });
+            $accepted = false;
 
-            // Randomly accept an answer for some questions
+            Post::factory(random_int(0, 5))->create(['question_id' => $question->id])->each(function ($answer) use ($accepted, $question) {
+                Comment::factory(random_int(0, 4))->create(['post_id' => $answer->id]);
+
+                // Randomly accept an answer for some questions
+                if (!$accepted && rand(0,5)) {
+                    $question->accepted_post_id = $answer->id;
+                    $question->save();
+                    $accepted = true;
+                }
+            });
         });
 
         // Update caches
